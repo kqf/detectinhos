fb76847f5522a2a3c2c60bcbb04d4c2e09c52e2a
kqf <q.dev.null@gmail.com>
2025-04-30 06:28:36 +0100

    Cover more scenarios with vanilla tests (#19)
    * Cover more scenarios with vanillaa tests
    
    * Fix names

--- tests/test_vanilla.py
+++ tests/test_vanilla.py
@@ -2,12 +2,29 @@ from functools import partial
 
 import torch
 
+from detectinhos.anchors import anchors
 from detectinhos.batch import detection_collate
 from detectinhos.sample import read_dataset
 from detectinhos.vanilla import DetectionDataset, DetectionTargets
 
 
-def test_vanilla(annotations):
+class DedetectionModel(torch.nn.Module):
+    def __init__(self, anchors: torch.Tensor, n_clases: int):
+        super().__init__()
+        self.register_buffer("anchors", anchors)
+        self.anchors: torch.Tensor = anchors
+        self.n_clases = n_clases
+
+    def forward(self, images: torch.Tensor) -> DetectionTargets:
+        batch = images.shape[0]
+        num_anchors = self.anchors.shape[0]
+        return DetectionTargets(
+            classes=torch.rand((batch, num_anchors, self.n_clases)),
+            boxes=torch.rand((batch, num_anchors, 4)),
+        )
+
+
+def test_vanilla(annotations, resolution=(480, 640)):
     dataloader = torch.utils.data.DataLoader(
         DetectionDataset(
             labels=read_dataset(annotations) * 8,
@@ -20,6 +37,18 @@ def test_vanilla(annotations):
             to_targets=DetectionTargets,
         ),
     )
+
+    model = DedetectionModel(
+        anchors=anchors(
+            min_sizes=[[16, 32], [64, 128], [256, 512]],
+            steps=[8, 16, 32],
+            clip=False,
+            resolution=resolution,
+        ),
+        n_clases=2,
+    )
+
     # sourcery skip: no-loop-in-tests
     for batch in dataloader:
-        print(batch.image.shape)
+        y_pred: DetectionTargets = model(batch.image)
+        print(y_pred)
