b92357d57a69e74c385d7f275baf2ac76780adb7
kqf <q.dev.null@gmail.com>
2025-05-06 07:20:20 +0200

    Remove unnecessary mock (#31)
    * Add the inference
    
    * Add the tests
    
    * Fix the vscode
    
    * Fix the samples
    
    * Fix the mocked test
    
    * Add the test

--- detectinhos/inference.py
+++ detectinhos/inference.py
@@ -12,9 +12,9 @@ def pred_to_labels(y_pred, priors) -> list[Sample]:
 
 def infer(
     image: np.ndarray,
-    to_image: Callable[np.ndarray, torch.Tensor],  # type: ignore
+    to_batch: Callable[np.ndarray, torch.Tensor],  # type: ignore
     model,
 ) -> list[Annotation]:
-    y_pred = model(to_image(image).unsqueeze(0))
+    y_pred = model(to_batch(image).unsqueeze(0))
     samples = pred_to_labels(y_pred, model.priors)
     return samples[0].annotations
