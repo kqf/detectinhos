d5c0b1a0b5c174b0a3f5608aee12d3e7d5683506
kqf <q.dev.null@gmail.com>
2025-05-06 07:18:25 +0200

    Add the test

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -5,17 +5,11 @@ import pytest
 import torch
 
 from detectinhos.inference import infer
-from detectinhos.sample import Annotation
 
 
 @pytest.fixture
 def image():
     return np.random.rand(480, 640, 3).astype(np.float32)
-
-
-@pytest.fixture
-def to_image():
-    return Mock(return_value=torch.rand(3, 480, 640))
 
 
 @pytest.fixture
@@ -26,16 +20,9 @@ def model():
     return mock_model
 
 
-@pytest.fixture
-def mock_pred_to_labels():
-    mock_annotations = [Annotation(label="test", bbox=(0, 0, 1, 1))]
-    mock_samples = [Mock(annotations=mock_annotations)]
-    mock_pred_to_labels = Mock()
-    mock_pred_to_labels.return_value = mock_samples
-    return mock_pred_to_labels
+def test_infer(image, to_image, model):
+    def to_batch(image):
+        return torch.from_numpy(image).permute(2, 0, 1).float()
 
-
-def test_infer(image, to_image, model, mock_pred_to_labels):
-    annotations = infer(image, to_image, model)
-    assert len(annotations) == 1
-    assert annotations[0].label == "test"
+    annotations = infer(image, to_batch, model)
+    assert len(annotations) == 0
