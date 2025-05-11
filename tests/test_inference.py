80c89c132d1148921113102a5b03da46b12fa769
kqf <q.dev.null@gmail.com>
2025-05-11 08:17:36 +0200

    Fix the anchor shape

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -16,7 +16,7 @@ def image():
 @pytest.fixture
 def model():
     mock_model = Mock()
-    mock_model.priors = torch.rand(1, 10, 4)
+    mock_model.priors = torch.rand(10, 4)
     classes = torch.rand(1, 10, 2)
     classes[:, :, 1] = 0.6
     mock_model.return_value = DetectionTargets(
