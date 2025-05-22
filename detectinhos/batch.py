a20392a4923cc98dba3e59766c5c3aacfd09d020
kqf <q.dev.null@gmail.com>
2025-05-22 06:41:37 +0100

    Add more logic to targets (#63)
    * Add a better interface
    
    * Add the metrics tests
    
    * Fix a typo
    
    * Fix the dataclass issue
    
    * Fix the prediction for tests
    
    * Fix the targets
    
    * FIx the metric calculation

--- detectinhos/batch.py
+++ detectinhos/batch.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, fields
 from typing import Callable, Generic, List, Optional, Protocol, TypeVar
 
+import numpy as np
 import torch
 from torch.nn.utils.rnn import pad_sequence
 
@@ -17,6 +18,9 @@ class HasBoxesAndClasses(Protocol, Generic[T]):
         ...
 
     def to_annotations(self) -> list[Annotation]:
+        ...
+
+    def to_numpy(self) -> "HasBoxesAndClasses[np.ndarray]":
         ...
 
 
@@ -50,6 +54,20 @@ class Batch:
                     annotations=pred[valid].to_annotations(),
                 )
             )
+        return output
+
+    def pred_to_numpy(
+        self,
+        select_valid_indices: Callable,
+    ) -> list[HasBoxesAndClasses[np.ndarray]]:
+        if self.pred is None:
+            return []
+
+        output = []
+        for batch_id, file in enumerate(self.files):
+            pred = self.pred[batch_id]
+            valid = select_valid_indices(pred)
+            output.append(pred[valid].to_numpy())
         return output
 
 
