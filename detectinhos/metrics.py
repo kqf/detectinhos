b36c79211e523e63a3e2f0048566421cf0df197e
kqf <q.dev.null@gmail.com>
2025-06-05 07:49:18 +0100

    Remove the printouts

--- detectinhos/metrics.py
+++ detectinhos/metrics.py
@@ -23,8 +23,6 @@ def prepare_outputs(
             ),
             axis=1,
         )
-        print(pred_sample)
-
         true_sample = np.zeros((true_.classes.shape[0], 7), dtype=np.float32)
         true_sample[:, :4] = true_.boxes
         # While calculating mAP, always start with 0
