2850d9a18e5af1b0fe5b2a8e333e2689a02297aa
kqf <q.dev.null@gmail.com>
2025-06-22 07:29:29 +0200

    Update the fixtures (#119)
    * Fix the metrics classes
    
    * Remove the ignore types
    
    * Add the inference to vanilla
    
    * XFail the problematic tests
    
    * Fix the pre-commits
    
    * Remove the inference from the metrics
    
    * Proper preds to logits
    
    * Restore the old tests

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,4 +1,3 @@
-from functools import partial
 from typing import Callable
 
 import numpy as np
@@ -37,28 +36,29 @@ def batch(
 def inference(pred, sample_anchors):
     # n_good_predictions = pred.shape[0]
 
-    # def dummy_decode(pred: DetectionTargets) -> torch.Tensor:
-    #     pred.boxes = decode(pred.boxes, sample_anchors, variances=[0.1, 0.2])
-    #     return pred[torch.arange(n_good_predictions)]
+    def _infer(batch: Batch) -> torch.Tensor:
+        batch.pred = on_batch(
+            batch=batch,
+            pipeline=compose(
+                to_numpy,
+                dummy_decode,
+            ),
+        )  # type: ignore
+        return batch
 
-    # def infer(batch: Batch) -> torch.Tensor:
-    #     batch.pred = on_batch(
-    #         batch=batch,
-    #         pipeline=compose(
-    #             to_numpy,
-    #             dummy_decode,
-    #         ),
-    #     )  # type: ignore
-    #     return batch
-    # return infer
-    return partial(infer, priors=sample_anchors)
+    return _infer
 
 
-@pytest.mark.xfail
+    # from detectinhos.vanilla import infer
+    # from functools import partial
+    # return partial(infer, priors=sample_anchors)
+
+    
+# @pytest.mark.xfail
 def test_mean_average_precision_add(
     batch: Batch,
     inference: Callable[..., torch.Tensor],
 ):
-    map_metric = MeanAveragePrecision(num_classes=2, inference=inference)
+    map_metric = MeanAveragePrecision(num_classes=2)
     map_metric.add(inference(batch))
     assert map_metric.value()["mAP"] == pytest.approx(0.5)
