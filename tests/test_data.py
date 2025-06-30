add4a5b2e9bc64edc02764f2750b4e7562f6e284
kqf <q.dev.null@gmail.com>
2025-06-30 06:57:27 +0200

    Fix the broken tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,7 @@
-from detectinhos.data import read_dataset
+from detectinhos.data import Annotation, Sample, read_dataset
 
 
 def test_reads_dataset(annotations):
-    dataset = read_dataset(annotations)
+    dataset = read_dataset(annotations, sample_type=Sample[Annotation])
     assert len(dataset) > 0
     assert len(dataset[0].annotations) > 0
