985d45805948eebe9949033d3af58f27e4f2a40c
kqf <q.dev.null@gmail.com>
2025-07-16 06:53:12 +0200

    Simplify the dataset

--- detectinhos/dataset.py
+++ detectinhos/dataset.py
@@ -33,10 +33,8 @@ class DetectionDataset(torch.utils.data.Dataset):
     def __init__(
         self,
         labels: list[Sample],
-        mapping: dict[str, int],
         transform: DatasetAugmentation = do_nothing,
     ) -> None:
-        self.mapping = mapping
         self.transform = transform
         self.labels = labels
 
@@ -45,11 +43,14 @@ class DetectionDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, index: int) -> BatchElement[torch.Tensor]:
         sample = self.labels[index]
-        image = load_rgb(sample.file_name)
-        targets = to_targets(sample, self.mapping)
-        image_t, targets_t = self.transform(image, targets)
+        raw_image = load_rgb(sample.file_name)
+        raw_targets = to_targets(sample, {})
+        image, targets = self.transform(
+            raw_image,
+            raw_targets,
+        )
         return BatchElement(
             file=sample.file_name,
-            image=image_t,
-            true=targets_t,
+            image=image,
+            true=targets,
         )
