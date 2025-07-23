64709822ed1b0b99f778f2195658383920c63fdc
kqf <q.dev.null@gmail.com>
2025-07-23 07:07:15 +0200

    Use the uniform approx

--- tests/test_vanilla.py
+++ tests/test_vanilla.py
@@ -18,6 +18,10 @@ from detectinhos.vanilla import (
 )
 
 
+def approx(x):
+    return pytest.approx(x, abs=0.001)
+
+
 class DedetectionModel(torch.nn.Module):
     anchors: torch.Tensor
 
@@ -117,8 +121,5 @@ def test_vanilla(batch_size, annotations, build_model, resolution=(480, 640)):
     sample = infer_on_rgb(np.random.randint(0, 255, resolution + (3,)))
     assert len(sample.annotations) == 1
     assert sample.annotations[0].label == "apple"
-    assert sample.annotations[0].score == pytest.approx(0.62, abs=0.001)
-    assert sample.annotations[0].bbox == pytest.approx(
-        [413.0, 390.0, 515.0, 459.0],
-        abs=0.001,
-    )
+    assert sample.annotations[0].score == approx(0.62)
+    assert sample.annotations[0].bbox == approx([413.0, 390.0, 515.0, 459.0])
